1baaac3e46e1ddc40cb5e0c7a6b16fed2fa05d0f
Richy <richymbiamou52@gmail.com>
2023-03-01 06:09:21 +0100

    fin partie2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ContactComponent} from "./contact/contact.component";
-
+import {PlanComponent} from "./plan/plan.component";
+import {AboutComponent} from "./about/about.component";
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'courses', component: ContactComponent},
+  {path: 'plan', component: PlanComponent},
+  {path: 'about', component: AboutComponent},
   {path: '', component: HomeComponent},
   {path: '**', redirectTo: '/home'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { PlanComponent } from './plan/plan.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    PlanComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
