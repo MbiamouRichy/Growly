8978bd03c2be6b4048faac856b7aa33b198a5795
Richy <richymbiamou52@gmail.com>
2023-03-02 18:49:38 +0100

    responsive  3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,27 @@ export class AppComponent {
     nav.forEach(a=>a.addEventListener('click', ()=>{
       nav.forEach(a=>a.classList.remove('active'));
       a.className +=' active';
-    }))
+    }));
+    let btn_hamburger = document.querySelector('.btn_menu_hamburger');
+    // @ts-ignore
+    btn_hamburger.addEventListener('click', () =>{
+      let navbar_mobile = document.querySelector('header nav');
+      // @ts-ignore
+      if ( navbar_mobile.style.display=='flex'){
+        // @ts-ignore
+        btn_hamburger.classList.remove('active')
+        document.body.style.overflow="auto"
+        // @ts-ignore
+        navbar_mobile.style.display="none";
+        document.body.style.userSelect='none'
+      }else{
+        document.body.style.overflow="hidden"
+        document.body.style.userSelect='auto';
+        // @ts-ignore
+        navbar_mobile.style.display='flex'
+        // @ts-ignore
+        btn_hamburger.classList.add('active')
+      }
+    })
   }
 }
