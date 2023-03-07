86b259de56526e0cb660e046c0ab033081c161b6
Richy <richymbiamou52@gmail.com>
2023-03-07 11:03:56 +0100

    responsive  4 : courses debut ( < 1000px )

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-
+  ngOnInit(){
+    let h3 = document.querySelector('.div_row2_col2 h3 br') as HTMLBRElement;
+    if (document.body.offsetWidth < 1108){
+      h3.style.display = "none";
+    }
+  }
 }
